71eb66d62cf710e3fb673e222951c50cf0c122d0
Lucius444 <furkan_cck@hotmail.com>
2021-10-15 09:43:20 +0300

    Initial Commit

---
+++ src/main/java/com/id3/activemq/ActivemqApplication.java
@@ -0,0 +1,15 @@
+package com.id3.activemq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jms.annotation.EnableJms;
+
+@SpringBootApplication
+@EnableJms
+public class ActivemqApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ActivemqApplication.class, args);
+    }
+
+}
